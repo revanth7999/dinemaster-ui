cd41df3f9c171452b7452daa19ba6868854ffa91
revanth7999 <revanthkumar.iris@gmail.com>
2025-11-26 12:21:26 +0530

    Implemented React.lazy mechanism

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,9 +1,13 @@
-import { Component } from "react";
+import { Component, Suspense, lazy } from "react";
 import { Routes, Route, Navigate } from "react-router-dom";
 import Landing from "./landing/Landing";
-import LoginForm from "./login/Login";
 import NewUser from "./newUser/NewUser";
 import Admin from "./admin/Admin";
+
+const LoginForm = lazy(() => import("./login/Login"));
+const Restaurants = lazy(() => import("./restaurants/Restaurants"));
+const OAuthSuccess = lazy(() => import("./oauth-success"));
+
 import {
   ADMIN_LANDING_PAGE,
   BASE_PAGE_PATH,
@@ -13,24 +17,26 @@ import {
   OAUTH_PAGE,
   RES_PAGE,
 } from "./Constants";
-import OAuthSuccess from "./oauth-success";
-import Restaurants from "./restaurants/Restaurants";
 import AuthLayout from "./AuthLayout";
 
 class Navigation extends Component {
   render() {
     return (
-      <Routes>
-        <Route path={BASE_PAGE_PATH} element={<Navigate to={LOGIN_PAGE} />} />
-        <Route path={LOGIN_PAGE} element={<LoginForm />} />
-        <Route path={CREATE_USER_PAGE} element={<NewUser />} />
-        <Route element={<AuthLayout />}>
-          <Route path={RES_PAGE} element={<Restaurants />} />
-          <Route path={LANDING_PAGE} element={<Landing />} />
-          <Route path={ADMIN_LANDING_PAGE} element={<Admin />} />
-          <Route path={OAUTH_PAGE} element={<OAuthSuccess />} />
-        </Route>
-      </Routes>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          <Route path={BASE_PAGE_PATH} element={<Navigate to={LOGIN_PAGE} />} />
+          
+          <Route path={LOGIN_PAGE} element={<LoginForm />} />
+          <Route path={CREATE_USER_PAGE} element={<NewUser />} />
+
+          <Route element={<AuthLayout />}>
+            <Route path={RES_PAGE} element={<Restaurants />} />
+            <Route path={LANDING_PAGE} element={<Landing />} />
+            <Route path={ADMIN_LANDING_PAGE} element={<Admin />} />
+            <Route path={OAUTH_PAGE} element={<OAuthSuccess />} />
+          </Route>
+        </Routes>
+      </Suspense>
     );
   }
 }
